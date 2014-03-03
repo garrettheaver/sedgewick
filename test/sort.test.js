4b56daf25dd4ce26070fa547ec86cde8628586c7
Garrett Heaver <garrett@iterationfour.com>
2014-03-03 18:35:58 +0000

    added ability to pass a custom sort comparator

--- test/sort.test.js
+++ test/sort.test.js
@@ -1,7 +1,7 @@
 var expect = require("chai").expect;
 var input = [4, 8, 2, 6, 1, 5, 3, 7];
 
-var itBehavesLikeASortAlgorithm = function(sort) {
+var itBehavesAsASort = function(sort) {
 
   it("happily sorts an empty array", function() {
     var output = sort([]);
@@ -13,9 +13,14 @@ var itBehavesLikeASortAlgorithm = function(sort) {
     expect(input).deep.equal([4, 8, 2, 6, 1, 5, 3, 7]);
   });
 
-  it("returns an new output array in natural order", function() {
+  it("sorts a new output array using natural order", function() {
     var output = sort(input);
     expect(output).deep.equal([1, 2, 3, 4, 5, 6, 7, 8]);
+  });
+
+  it("sorts a new output array using custom order", function() {
+    var output = sort(input, function(a, b) { return a > b; });
+    expect(output).deep.equal([8, 7, 6, 5, 4, 3, 2, 1]);
   });
 
 };
@@ -23,14 +28,14 @@ var itBehavesLikeASortAlgorithm = function(sort) {
 describe("selectionSort", function() {
 
   var sort = require("../lib/selectionSort");
-  itBehavesLikeASortAlgorithm(sort);
+  itBehavesAsASort(sort);
 
 });
 
 describe("insertionSort", function() {
 
   var sort = require("../lib/insertionSort");
-  itBehavesLikeASortAlgorithm(sort);
+  itBehavesAsASort(sort);
 
 });
 
