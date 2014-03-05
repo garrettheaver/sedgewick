412b7ddd83ce983b4eb1cd8f162c92c16e5d9e64
Garrett Heaver <garrett@iterationfour.com>
2014-03-05 20:18:24 +0000

    added tests for alphabetical sorting of strings

--- test/sort.test.js
+++ test/sort.test.js
@@ -3,7 +3,8 @@ var expect = require("chai").expect;
 var itBehavesAsASort = function(sort) {
 
   var options = require("../lib/sortOptions"),
-      input = [4, 8, 2, 6, 1, 9, 5, 3, 0, 7];
+      numbers = [4, 8, 2, 6, 1, 9, 5, 3, 0, 7],
+      alpha = ['gb', 'ie', 'ga', 'de', 'fr'];
 
   it("happily sorts an empty array", function() {
     var output = sort([]);
@@ -11,18 +12,23 @@ var itBehavesAsASort = function(sort) {
   });
 
   it("does not modify the original input array", function() {
-    var output = sort(input);
-    expect(input).deep.equal([4, 8, 2, 6, 1, 9, 5, 3, 0, 7]);
+    var output = sort(numbers);
+    expect(numbers).deep.equal([4, 8, 2, 6, 1, 9, 5, 3, 0, 7]);
   });
 
   it("sorts a new output array using natural order", function() {
-    var output = sort(input);
+    var output = sort(numbers);
     expect(output).deep.equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 
   it("sorts a new output array using custom order", function() {
-    var output = sort(input, options.byDescendingOrder);
+    var output = sort(numbers, options.byDescendingOrder);
     expect(output).deep.equal([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]);
+  });
+
+  it("sorts strings in alphabetical order", function() {
+    var output = sort(alpha);
+    expect(output).deep.equal(['de', 'fr', 'ga', 'gb', 'ie']);
   });
 
   it("sorts odd sized arrays", function() {
